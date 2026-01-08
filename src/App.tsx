c14bdf033fa81c2779646ebbbb6c0fa4cc4eec6f
Garrett Voss <garrettvoss4@gmail.com>
2026-01-08 14:07:17 -0600

    feat: basic setup for hitting local backend

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  async function fetchData() {
+   const response = await fetch('/bridge/getBridgesInDeck', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify([{
+        name: 'Effect Veiler',
+        race: 'Spellcaster',
+        attribute: 'LIGHT',
+        level: 1,
+        atk: 0,
+        def: 0,
+      },
+      {
+        name: 'Blue-Eyes White Dragon',
+        race: 'Dragon',
+        attribute: 'LIGHT',
+        level: 8,
+        atk: 3000,
+        def: 2500,
+      }
+    ]),
+    });
+    return await response.json();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={() => fetchData()}>Fetch Data</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
